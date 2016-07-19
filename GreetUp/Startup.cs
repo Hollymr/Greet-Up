298ecde3d8777fa567104e4b872672f22f13be33
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-19 14:36:31 -0400

    inital commit

---
+++ GreetUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreetUp.Startup))]
+namespace GreetUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
